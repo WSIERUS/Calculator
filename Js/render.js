ef7f0e5d02bfb7878d3f290c8b8859f955bbdea4
Wojciech <wojt.sier@o2.pl>
2022-02-22 22:34:12 +0100

    1.5 Styled

--- Js/render.js
+++ Js/render.js
@@ -10,7 +10,7 @@ buttons.map(button => { // Mapowanie przycisków dla check listy
     input.setAttribute('type', 'checkbox')
     input.checked = button.visible
     const span = document.createElement('span')
-    span.textContent = `Aktywuj ${button.name}`
+    span.textContent = `Aktywuj "${button.name}"`
     const label = document.createElement('label')
     label.className = 'check-list-label'
     label.appendChild(input)
@@ -32,13 +32,23 @@ controlPanel.appendChild(acceptButton)
 
 const isControlPanelVisible = () => { // Obsługa zaakaceptowanie wyboru checklisty oraz mapowanie calculatora
   controlPanel.className = 'hidden'
-  buttons.map(button => {
+
+  buttons.map(button => { // Mapowanie przycisków kalkulatora
     const element = document.createElement('button')
     element.textContent = button.name
     element.className = button.visible ? button.class : `${button.class}` + ' unvisible'
-    if (button.visible) element.addEventListener('click', () => operationButtonType(button.type, button.value, button.display))
+
+    if (button.visible) { // Funkcje dla aktywnego przycisku
+      element.addEventListener('click', () => operationButtonType(button.type, button.value, button.display))
+      element.addEventListener('mouseenter', () => handleMouseMoveOnButton(true, element, button.class))
+      element.addEventListener('mouseleave', () => handleMouseMoveOnButton(false, element, button.class))
+    }
     calculator.appendChild(element)
   })
+}
+
+const handleMouseMoveOnButton = (isMoved, button, clas) => { // Stylowanie przycisków w czasie rzeczywistym
+  button.className = isMoved ? `${clas}` + ' moved' : clas
 }
 
 const renderDisplay = () => { // Renderowanie wyniku na wyświetlaczu
